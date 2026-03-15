d4f3d29bbb3ea627084f8f4053a5e55ea6bbecf4
Andreas Gabel <andreassgabel@hotmail.com>
2026-03-15 17:09:56 +0100

    wire up endpoint explorer in HTML, app.js and i18n

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,5 +1,6 @@
 import * as I18n from './i18n.js';
 import * as Documents from './documents.js';
+import * as Endpoints from './endpoints.js';
 import './projects.js';
 import './exit-intent.js';
 import './modal.js';
@@ -7,6 +8,7 @@ import './modal.js';
 async function boot() {
     await I18n.load(I18n.getLang());
     Documents.init();
+    Endpoints.init();
 }
 
 document.getElementById('lang-toggle').addEventListener('click', async () => {
